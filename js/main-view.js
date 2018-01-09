a1389a1a8a7d6957ce2872ffdb8796d7814c3621
AraceliGS <aragutarras@gmail.com>
2018-01-09 17:19:04 -0500

    añadiendo avance en los estilos y la funcionalidad

--- js/main-view.js
+++ js/main-view.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
+  var $textArea = $('#exampleTextArea1');
+  var $button = $('#publishButton');
+  $button.on('click', function(event) {
+    event.preventDefault();
+    var $divsContainer = $('#divs-container');
+    var $div = $('<div></div>');
+    var $post = $textArea.val();
+    $div.append($post);
+    $divsContainer.append($div);
+    $div.addClass('divText');
+  });
 });
