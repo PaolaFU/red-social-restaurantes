80828ba9de4b58d59d52bab3b7a356ff9aa800fc
AraceliGS <aragutarras@gmail.com>
2018-01-10 15:13:32 -0500

    corrigiendo errores en el menú desplegable donde se muestran las modificaciones y empezando a crear un chat

--- js/main-view.js
+++ js/main-view.js
@@ -7,10 +7,12 @@ $(document).ready(function() {
   var $dropdown = $('#dropdown');
   var $newFriend1 = $('#confirmButton1');
   var $newFriend2 = $('#confirmButton2');
+  var $ul = $('#dropdown-menu-followers');
   var $liNewFriend1 = $('#liNewFriend1');
   var $liNewFriend2 = $('#liNewFriend2');
   var $liMessage = $('#liMessage');
-  var $ul = $('#dropdown-menu-followers');
+  var $input = $('#exampleInputFile');
+  var $saveChanges = $('#button-save-changes');
   // Evento para el elemento donde se postean las reseñas
   $textArea.on('input', function() {
     if ($(this).val() !== '') {
@@ -37,19 +39,32 @@ $(document).ready(function() {
     $newFriend1.mouseover(function() {
       $newFriend1.click(function(event) {
         event.preventDefault();
-        $alert = alert('¡Ahora son amigas!');
         $liNewFriend1.toggle();
+        $alert = alert('¡Ahora son amigas!');
+        $alert.one();
+        $(this).off(event);
       });
     });
     $newFriend2.mouseover(function() {
       $newFriend2.click(function(event) {
         event.preventDefault();
-        $alert = alert('¡Ahora son amigos!');
         $liNewFriend2.toggle();
-      }); 
-    }); 
+        $alert = alert('¡Ahora son amigos!');
+        $alert.one();
+        $(this).off(event);
+      });
+    });
     if ($liNewFriend1.html() === '' || $liNewFriend2.html() === '') {
       $ul.addClass('dropdown-menu');
     }
+  });
+  // Evento aplicado al botón del modal para subir las imágenes
+  var $inputImage = $('#inputImage');
+  $saveChanges.on('click', function() {
+    debugger;
+    var $userImage = $input.val();
+    var $inputImageSrc = $inputImage.val();
+    $inputImageSrc = $userImage;
+    alert($inputImageSrc);
   });
 });
