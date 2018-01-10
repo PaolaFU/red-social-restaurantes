7ad47d9fbf307e7a2225ec9fc25f9cc8e90eb35d
AraceliGS <aragutarras@gmail.com>
2018-01-10 00:03:46 -0500

    añadiendo funcionalidfad al contenedor de los posts

--- js/main-view.js
+++ js/main-view.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
   var $textArea = $('#exampleTextArea1');
   var $button = $('#publishButton');
-  $button.on('click', function(event) {
-    event.preventDefault();
-    var $divsContainer = $('#divs-container');
-    var $div = $('<div></div>');
-    var $post = $textArea.val();
-    $div.append($post);
-    $divsContainer.append($div);
-    $div.addClass('divText');
+  // Eventos
+  $textArea.on('input', function() {
+    if ($(this).val() !== '') {
+      console.log($(this).val().length);
+      $button.removeAttr('disabled');
+    } else {
+      $button.attr('disabled', true);
+    }
   });
+  if ($button.attr('disabled') !== true) {
+    $button.on('click', function(event) {
+      event.preventDefault();
+      var $divsContainer = $('#divs-container');
+      var $div = $('<div></div>');
+      var $post = $textArea.val();
+      $div.append($post);
+      $divsContainer.append($div);
+      $div.addClass('divText');
+    });
+  }
 });
+// console.log($(this).val().length);
+// if ($(this).val().length < 1) {
+//   $button.attr('disabled', true);
+// }
