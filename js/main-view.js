82a68a962859baa101e51e890a234c2d37517689
AraceliGS <aragutarras@gmail.com>
2018-01-10 01:37:45 -0500

    añadiendo avances

--- js/main-view.js
+++ js/main-view.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+  var $followersDropdown = $('#followers');
+  var $notificationsDropdown = $('#notifications');
   var $textArea = $('#exampleTextArea1');
   var $button = $('#publishButton');
-  // Eventos
+  // // Eventos
+  // $followersDropdown.on('click', function() {
+  //   // var $followersDiv = $('#followersDiv');
+  //   var $followersContainer = $('<div></div>');
+  //   $followersDiv.append($followersContainer);
+  // });
   $textArea.on('input', function() {
     if ($(this).val() !== '') {
       console.log($(this).val().length);
