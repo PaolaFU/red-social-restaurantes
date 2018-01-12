f86e11142fa41911ad7bc1077a41f8acf93dbeb7
AraceliGS <aragutarras@gmail.com>
2018-01-12 08:58:28 -0500

    añadiendo avances en la funcionalidad del chat

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+  var $startConversationButton = $('#startConversation');
+  $startConversationButton.on('click', function() {
+    var $ws;
+    $ws = new WebSocket('wss://echo.websocket.org');
+    $ws.onopen = function() {
+      alert('Conexión abierta. Puedes enviar mensajes.');
+    };
+  
+    $ws.onmessage = function(messages) {
+      var $obtainedMessages = messages.data;
+      alert('Mensajes obtenidos son:' + $obtainedMessages);
+    };
+  
+    $ws.onclose = function() {
+      alert('Conexion cerrada');
+    };
+  });
 });
