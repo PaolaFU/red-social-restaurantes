80cc874a339c0990e32fb1a3bc50a2672830aaaa
PaolaFU <paola_sfu@hotmail.com>
2018-01-12 01:21:17 -0500

    Modificando login.js y login.html

--- js/login.js
+++ js/login.js
@@ -1,136 +1,46 @@
 $(document).ready(function() {
-  /* var $name = $('#name');
-  var $emailLogin = $('#email-login');
-  var $passwordLogin = $('#password-login');
-  var $emailRegister = $('#email-register');
-  var $passwordregister = $('#password-register');
-  var $checked = $('input[type="checkbox"]');
-    
-  var validateEmailLogin = false;
-  var validateEmailRegister = false;
-  var validateName = false; 
-  var validateChecked = false;  
-  
-  // para ingresar un correo electronico y validarlo
-  $emailLogin .on('input', function(event) {
-    var errorMessage = $('#error-message');
-    var txtMenssage = '<h5 style=\'color:#FF1493\';>Ingrese un correo electronico valido por ejemplo: usuario@dominio.com</h5>';
-    
-    var REGEXEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    if (REGEXEMAIL.test($(this).val())) {
-      validateEmail = true;
-      activeButton(); 
-    } else {
-      errorMessage.html(txtMenssage); 
-      desactiveButton();
-    }
-  });
-  $emailRegister .on('input', function(event) {
-    // var errorMessage = $('#error-message');
-    // var txtMenssage = '<h5 style=\'color:#FF1493\';>Ingrese un correo electronico valido por ejemplo: usuario@dominio.com</h5>';
-    
-    var REGEXEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    if (REGEXEMAIL.test($(this).val())) {
-      validateEmail = true; 
-      activeButton(); 
-    } else {
-      errorMessage.html(txtMenssage); 
-      desactiveButton();
-    }
-  });
-  
-  // Para validar nombre
-  $name.on('input', function() {
-    // console.log($(this).val());
-    if ($(this).val().length >= 3) {
-      validateName = true;
-      activeButton(); 
-    } else {
-      desactiveButton(); 
-    }
-  });
-   
-
-  // Para pasar a la siguiente vista 
-  $('#btn-login').on('click', function(event) {
-    window.location.href = 'main-view.html';
-  });
-  $('#btn-register').on('click', function(event) {
-    alert('Ha ocurrido un error');
-    window.location.href = 'main-view.html';
-    
-  });*/
-  //-------------------------------------------FIREBASE------------------------------------------------------//
-  // Login
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyCdxx880m4Ezk_Dlc84mE2zIrMe2lzH2Ek',
+    authDomain: 'misky-peru-57ca6.firebaseapp.com',
+    databaseURL: 'https://misky-peru-57ca6.firebaseio.com',
+    projectId: 'misky-peru-57ca6',
+    storageBucket: 'misky-peru-57ca6.appspot.com',
+    messagingSenderId: '157888336080'
+  };
+  firebase.initializeApp(config);
   
   $('#btn-login').click(function() {
-    var emailLogin = document.getElementById('email-login').value;
-    var passwordLogin = document.getElementById('password-login').value;
-    
-    
-    firebase.auth().signInWithEmailAndPassword(emailLogin, passwordLogin).catch(function(error) {
-    // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    
-      console.log(errorCode);
-      console.log(errorMessage);
-    });
+    firebase();
   });
-  // Register
-  $('#btn-register').click(function() {
-    var emailRegister = document.getElementById('email-register').value;
-    var passwordRegister = document.getElementById('password-register').value;
-  
-    firebase.auth().createUserWithEmailAndPassword(emailRegister, passwordRegister)
-      .then(function() {
-        verificar();
-      })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-  });
-
-  function observador() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log('Tu correo ya esta registrado');
-        aparece(user);
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        console.log('**************');
-        console.log(user.emailVerified);
-        console.log('**************');
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-      // ...
-      } else {
-      // User is signed out.
-        console.log('no existe usuario activo');
-      // ...
-      }
+   
+  function firebase() {
+    // provedor del servicio
+    // con qué servicio se está rergistrando el usuario
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // result trae toda la información de gmail
+    // levantar la ventana de gmail y junto a result
+    // popup es la ventana emergente que te indica con qué cuenta estás ingresando
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // guardando la imagen y nombre
+      localStorage.photo = result.user.photoURL; // recoge foto de usuario
+      localStorage.name = result.user.displayName; // recoge nombre
+      localStorage.id = result.user.uid; // código de usurio de gmail
+      console.log(result.user);
+      saveFirebase(result.user); 
     });
-  }
-  observador();
-  
-  function verificar() {
-    var user = firebase.auth().currentUser;
 
-    user.sendEmailVerification().then(function() {
-    // Email sent.
-      alert('Enviando correo de verificación, una vez verificado su correo inicie sesión');
-    }).catch(function(error) {
-    // An error happened.
-      alert('Ha ocurrido un error');
-    });
+      // funcion para guardar en firebase los datos de quien entra
+    function saveFirebase(user) {
+    var user = {
+      uid: user.uid,
+      name: user.displayName,
+      photo: user.photoURL,
+      gmail: user.email,
+      followers: 31,
+    };
+    firebase.database().ref('users/' + user.uid).set(user);
+    // window.location.href = '../views/profile.html';
   }
+};
 });
