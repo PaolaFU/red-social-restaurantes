3c7828cb9f98e72037c5b91ff0fbcb144d27a473
PaolaFU <paola_sfu@hotmail.com>
2018-01-12 04:21:09 -0500

    Arreglando estilos y funcionalidad

--- js/perfil.js
+++ js/perfil.js
@@ -5,36 +5,36 @@ $(document).ready(function() {
   });
   $('#npost').on('click', function() {
     // Atrapa el texto ingresado
-    let comments = document.getElementById('comment').value;
+    var comments = document.getElementById('comment').value;
   
     // limpiar el textarea
     document.getElementById('comment').value = '';
   
     // contenedor en el html
-    let cont = document.getElementById('cont');
+    var cont = document.getElementById('cont');
   
     // creacion del div que contiene cada comentario
-    let newComments = document.createElement('div');
+    var newComments = document.createElement('div');
   
     // validacion de  textarea con mensaje
     if (comments.length === 0 || comments === null) {
       return false;
     }
     // creacion del checkbox
-    let chck = document.createElement('input');
-    chck.type = 'checkbox';
+    var chck = document.createElement('input');
+    chck.type = 'text';
     // creacion icono de heart
-    let heart = document.createElement('i');
+    var heart = document.createElement('i');
     heart.classList.add('fa', 'fa-heart', 'heart');
     // creacion icono de basura
-    let trash = document.createElement('i');
+    var trash = document.createElement('i');
     trash.classList.add('fa', 'fa-trash', 'trash');
     // nodos de texto del textarea
     var textNewComment = document.createTextNode(comments);
   
-    let contenedorElemento = document.createElement('p');
+    var contenedorElemento = document.createElement('p');
     contenedorElemento.appendChild(textNewComment);
-    newComments.appendChild(chck);
+    // newComments.appendChild(chck);
     newComments.appendChild(trash);
     newComments.appendChild(heart);
     newComments.appendChild(contenedorElemento);
